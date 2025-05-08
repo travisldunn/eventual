2eb051d683c285317428fccdce1a8b385a49b223
travisldunn <travisldunn@gmail.com>
2025-05-08 18:52:50 -0400

    Fix base path for GitHub Pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,8 @@ export default defineConfig({
     host: "0.0.0.0",
     port: 8080,
   },
-  plugins: [
-    react(),
-  ],
+  base: "/eventual/",
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
